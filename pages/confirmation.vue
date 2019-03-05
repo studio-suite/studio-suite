0e6f4957eabd9fd591afdd7e03ed35c23c7091d9
raducretu <ameriza@gmail.com>
2019-03-05 23:26:41 +0200

    asd

--- pages/confirmation.vue
+++ pages/confirmation.vue
@@ -1,13 +1,5 @@
 <template>
     <div>
-        <div class="confirmation-wrapper">
-            <div class="confirmation">
-                <h2>Registration Confirmation</h2>
-                <img src="~/assets/ok.svg">
-                <p class="text-align--center margin-top--4" v-if="booking"><template v-if="booking.price > 0">Your payment of <strong>{{ booking.attendees.length * booking.price | currency(tenantCurrency) }}</strong> processed successfully.</template> We sent you an email confirmation.</p>
-            </div>
-        </div>
-        <!--<a href="#" v-on:click.prevent="$router.go(-1)">Back to schedules</a>-->
         <modal name="confirmation-modal" height="auto" :adaptive="true" classes="confirmation-modal" @before-close="allowCloseModal">
             <div class="text-align--right"><i class="fal fa-times" v-on:click.prevent="closeModal"></i></div>
             <div class="confirmation">
